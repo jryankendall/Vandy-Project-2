7ff7ab6561830cc81499ebe635e4ef79cf481a8a
unknown <jed@jedkendall.com>
2019-05-02 15:21:38 -0500

    Fixed ESLint Issues

--- public/js/newsfeed.js
+++ public/js/newsfeed.js
@@ -11,7 +11,7 @@ var NewsObject = function(appnews) {
         var articleCount = 5;
         if (this.count < 5) {
             articleCount = this.count;
-        };
+        }
         for (var i = 0; i < articleCount; i++) {
             //Prints the most recent 5 articles for the app, or all of them if there's fewer than 5
             var newsItem = this.newsItems[i];
@@ -36,9 +36,25 @@ var NewsObject = function(appnews) {
 
             itemDiv.append(titleDiv).append(contentsDiv);
             $("#newsfeed-div").append(itemDiv); //the div with the id #newsfeed-div will need to be put into the html by the front end developer
-        };
+        }
     };
 };
+
+//This is just so eslint stops throwing a fit about unused variables, despite the fact it's a #@*$! constructor to be used in a different file. whatever.
+var testNews = new NewsObject( {
+    appid: 69,
+    newsItems: [{
+        url: "ACK",
+        title: "It is what it is",
+        feedlabel: "nowhere",
+        date: 500,
+        contents: "BEHOLD! Article would go here."
+    }],
+    count: 1
+});
+
+console.log(testNews);
+
 
 //In the page Ajax logic (load AFTER this file), the API call will be expecting the appnews object from the back-end API, and will construct a new NewsObject with it, then execute the NewsObject.printNews() function
 //Well, it should, assuming I wrote it. If you're writing it, just make sure you make sure to not mix up parameters and such
