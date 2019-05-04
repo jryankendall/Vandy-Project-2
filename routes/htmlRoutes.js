c0b703203fcf23715c92ce148959255a28fe3c06
Sasha Peters <peteram1@sewanee.edu>
2019-05-04 09:46:22 -0500

    login field added and createAccount empty page added

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -33,6 +33,10 @@ module.exports = function(app) {
         res.render("user");
     });
 
+    app.get("/createAccount", function(req,res){
+        res.render("createAccount");
+    });
+
     // Render 404 page for any unmatched routes
     app.get("*", function(req, res) {
         res.render("404");
