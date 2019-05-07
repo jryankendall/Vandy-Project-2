e9768f2c932dbe58d4c25d087da0e5383a06bb1c
Sasha Peters <peteram1@sewanee.edu>
2019-05-07 13:52:46 -0500

    checking req

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,17 +12,24 @@ module.exports = function (app) {
     app.post("/api/userdetails",function (req, res) {
         console.log(req.body);
         console.log(req.session.passport.user.profile.id);
-        db.users.findOne({ where: {username: req.body.username} }).then(function(dbUser){
-            if(!dbUser){
-                db.users.update(
-                    {username: req.body.username, description: req.body.description},
-                    {where: {email: req.session.passport.user.profile.id}})
-                    .then(function () {
-                        res.json({success: "User details successfully updates!(doesn't yet)"});
-                    });
+        db.users.findOne({ where: {email: req.session.passport.user.profile.id} }).then(function(dbUserCheck){
+            if(!dbUserCheck){
+                res.redirect("/user");
             }
             else{
-                res.json({success: "This username already exists.(won't until criteria met)"});
+                db.users.findOne({ where: {username: req.body.username} }).then(function(dbUser){
+                    if(!dbUser){
+                        db.users.update(
+                            {username: req.body.username, description: req.body.description},
+                            {where: {email: req.session.passport.user.profile.id}})
+                            .then(function () {
+                                res.redirect("/user");
+                            });
+                    }
+                    else{
+                        res.json({success: "This username already exists.(won't until criteria met)"});
+                    }
+                });
             }
         });
     });
