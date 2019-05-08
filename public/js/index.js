2986528bfffbb84224e7b0a38a8c4174c303e9ff
Sasha Peters <peteram1@sewanee.edu>
2019-05-08 14:21:20 -0500

    search person test2

--- public/js/index.js
+++ public/js/index.js
@@ -53,7 +53,7 @@ var handlePersonSearch = function (event) {
         return;
     }
 
-    API.searchGame(game).then(function (data) {
+    API.searchGame(person).then(function (data) {
         //call function to do something with API data
         console.log(data);
         //displaySearchResults(data);
