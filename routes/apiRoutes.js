f78d191507bd31b5cbad79efb466ea8f62555490
Sasha Peters <peteram1@sewanee.edu>
2019-05-08 14:53:50 -0500

    search person test6

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -70,10 +70,15 @@ module.exports = function (app) {
     app.get("/api/person/search/:person", function (req, res){
         var username = req.params.person;
         db.users.findOne({ where: { username: username } }).then(function (dbUsers) {
-            dbUsers.dataValues.email = "not passed";
-            dbUsers.dataValues.createdAt = "not passed";
-            dbUsers.dataValues.updatedAt = "not passed";
-            res.json(dbUsers.dataValues);
+            if(!dbUsers){
+                res.json("User not found");
+            }
+            else {
+                dbUsers.dataValues.email = "not passed";
+                dbUsers.dataValues.createdAt = "not passed";
+                dbUsers.dataValues.updatedAt = "not passed";
+                res.json(dbUsers.dataValues);
+            }
         });
     });
 
